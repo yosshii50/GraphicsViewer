811ee339e9839c27998ffb4d90dafc093f9cb409
Yagyos <Yagyos@users.noreply.github.com>
2019-11-14 11:16:06 +0900

    Provisional

--- GraphicsViewer/Form1.Designer.vb
+++ GraphicsViewer/Form1.Designer.vb
@@ -23,18 +23,30 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PictureBox1
         '
         Me.PictureBox1.BackColor = System.Drawing.Color.Black
-        Me.PictureBox1.Location = New System.Drawing.Point(62, 55)
+        Me.PictureBox1.Location = New System.Drawing.Point(23, 32)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(448, 433)
+        Me.PictureBox1.Size = New System.Drawing.Size(239, 456)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.Black
+        Me.PictureBox2.Location = New System.Drawing.Point(328, 32)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(239, 456)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
         '
         'Form1
         '
@@ -42,13 +54,16 @@ Partial Class Form1
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.AutoScroll = True
         Me.ClientSize = New System.Drawing.Size(669, 591)
+        Me.Controls.Add(Me.PictureBox2)
         Me.Controls.Add(Me.PictureBox1)
         Me.Name = "Form1"
         Me.Text = "Form1"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
     Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
 
 End Class
--- GraphicsViewer/Form1.vb
+++ GraphicsViewer/Form1.vb
@@ -1,18 +1,80 @@
 ﻿Public Class Form1
 
+    Dim FileNameList() As String
+    Dim NowPos As Integer
+
+    Private Sub Form1_Activated(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Activated
+
+
+        On Error Resume Next
+        'Dim WrkFileList As String() = System.IO.Directory.GetFiles("C:\!Delete\書籍１", "*.jpg")
+        FileNameList = System.IO.Directory.GetFiles("C:\!Delete\書籍１", "*.jpg")
+        On Error GoTo 0
+
+        NowPos = 0
+        Call ImageView(NowPos)
+
+    End Sub
+
+    Private Sub ImageView(ByRef WrkPos As Integer)
+
+        If WrkPos < 0 Then
+            WrkPos = 0
+        End If
+        If WrkPos >= FileNameList.Count Then
+            WrkPos = FileNameList.Count - 1
+        End If
+
+        PictureBox1.ImageLocation = FileNameList(WrkPos)
+        PictureBox2.ImageLocation = FileNameList(WrkPos + 1)
+        PictureBox1.Refresh()
+        PictureBox2.Refresh()
+        Me.Text = WrkPos + 1 & "/" & FileNameList.Count
+
+    End Sub
+
+    Private Sub Form1_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyDown
+        Select Case e.KeyCode
+            Case Keys.Left
+                NowPos = NowPos - 1
+                Call ImageView(NowPos)
+            Case Keys.Right
+                NowPos = NowPos + 1
+                Call ImageView(NowPos)
+            Case Keys.Up
+                NowPos = NowPos - 2
+                Call ImageView(NowPos)
+            Case Keys.Down
+                NowPos = NowPos + 2
+                Call ImageView(NowPos)
+            Case Keys.PageUp
+                NowPos = 0
+                Call ImageView(NowPos)
+            Case Keys.Next
+                NowPos = FileNameList.Count - 1
+                Call ImageView(NowPos)
+            Case Else
+                Debug.WriteLine(e.KeyCode)
+        End Select
+    End Sub
+
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
 
         PictureBox1.Top = 0
         PictureBox1.Left = 0
 
-        PictureBox1.ImageLocation = "C:\!Delete\書籍１\Page0015.jpg"
+        PictureBox2.Top = 0
 
     End Sub
 
     Private Sub Form1_Resize(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Resize
 
-        PictureBox1.Width = Me.ClientSize.Width
+        PictureBox1.Width = Me.ClientSize.Width / 2
         PictureBox1.Height = Me.ClientSize.Height
+
+        PictureBox2.Left = PictureBox1.Width
+        PictureBox2.Width = Me.ClientSize.Width / 2
+        PictureBox2.Height = Me.ClientSize.Height
 
     End Sub
 End Class
